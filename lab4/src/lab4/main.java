47fcc709e49195b21053e518d0620ad08b574ac5
smilezevil <demyanchuknastya2018@gmail.com>
2026-03-25 23:15:51 +0200

    add main for lab 4

---
+++ lab4/src/lab4/main.java
@@ -0,0 +1,17 @@
+package lab4;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Лабораторна робота №4");
+        new task1().run();
+        new task2().run();
+        new task3().run();
+        new task4().run();
+        new task5().run();
+        new task6().run();
+        new task7().run();
+        new task8().run();
+        new task9().run();
+        new task10().run();
+    }
+}
