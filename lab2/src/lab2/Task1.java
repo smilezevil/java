c20ebd88a9f6d93bf2adaa059ab98e5ca7bb6585
smilezevil <demyanchuknastya2018@gmail.com>
2026-04-16 11:55:17 +0300

    add all tasks for lab2

--- lab2/src/lab2/Task1.java
+++ lab2/src/lab2/Task1.java
@@ -7,28 +7,24 @@ public class Task1 {
     public void run() {
         System.out.println("Завдання 1: Обробка винятків");
 
-        // Тестуємо правильні дані
         System.out.println("Спроба входу з правильними даними:");
-        loginSystem("admin", "qwerty");
+        loginSystem("smilezevil", "smile0110");
 
         System.out.println("\nСпроба входу з неправильними даними:");
-        // Тестуємо неправильні дані, щоб викликати Exception
-        loginSystem("guest", "12345");
+        loginSystem("abobus", "12345");
     }
 
     public void loginSystem(String username, String password) {
-        String correctUsername = "admin";
-        String correctPassword = "qwerty";
+        String correctUsername = "smilezevil";
+        String correctPassword = "smile0110";
 
         try {
             if (!username.equals(correctUsername) || !password.equals(correctPassword)) {
-                // Викидаємо Exception, якщо дані невірні
                 throw new InvalidCredentialsException("Неправильне ім'я користувача або пароль для: " + username);
             }
             System.out.println("Успішний вхід! Вітаємо, " + username + ".");
 
         } catch (InvalidCredentialsException e) {
-            // Перехоплюємо Exception і відображаємо повідомлення
             System.out.println("Помилка авторизації: " + e.getMessage());
         }
     }
--- lab2/src/lab2/Task2.java
+++ lab2/src/lab2/Task2.java
@@ -10,20 +10,17 @@ public class Task2 {
   public void run() {
     System.out.println("\nЗавдання 2: Колекції");
 
-    // Використовуємо інтерфейс Queue та реалізацію LinkedList для черги FIFO
     Queue<String> customerQueue = new LinkedList<>();
 
     System.out.println("Додаємо запити до черги...");
-    customerQueue.add("Запит #1: Відновлення пароля");
-    customerQueue.add("Запит #2: Проблема з оплатою");
-    customerQueue.add("Запит #3: Консультація щодо товару");
+    customerQueue.add("Запит 1: Відновлення пароля");
+    customerQueue.add("Запит 2: Проблема з оплатою");
+    customerQueue.add("Запит 3: Консультація щодо товару");
 
     System.out.println("Поточна черга: " + customerQueue);
     System.out.println("Починаємо обробку...\n");
 
-    // Обробляємо запити: вилучаємо по одному, поки черга не стане порожньою
     while (!customerQueue.isEmpty()) {
-      // poll() дістає перший елемент і ВИДАЛЯЄ його з черги
       String currentRequest = customerQueue.poll();
       System.out.println("Обробляється: [" + currentRequest + "]");
     }
