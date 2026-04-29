9d07a3332bce09933af9a68548af595d23f31204
smilezevil <demyanchuknastya2018@gmail.com>
2026-04-29 13:22:28 +0300

    Add Service model with Lombok annotations

---
+++ hotel/src/main/java/com/hotel/model/Service.java
@@ -0,0 +1,14 @@
+package com.hotel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Service {
+    private Long id;
+    private String name;
+    private double price;
+}
